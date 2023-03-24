2fd6a7a2c04a06fc82272961cd8238099de06987
Brandon Simmons <bsimmons1@ycp.edu>
2023-03-24 17:15:33 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return
+
+
+if __name__ == '__main__':
+    app.run()
