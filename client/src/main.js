2eb016f025fc418697a5f529d30db76fdf0a0093
Brandon Simmons <bsimmons1@ycp.edu>
2023-03-24 23:21:36 -0400

    adding all modules

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
