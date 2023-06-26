53a9b294492f085fefefce97edf78d50a8911efe
Brandon Simmons <bsimmons@ycp.edu>
2023-06-25 20:47:41 -0400

    Converting project to have a clean rest api structure

--- app.py
+++ app.py
@@ -1,108 +1,11 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory
-from flask_cors import CORS
-import uuid
+from application import create_app
 
-# instantiate the app
-app = Flask(__name__, static_folder='./dist/static',
-            template_folder='./dist')
-app.config.from_object(__name__)
-
-# enable CORS
-CORS(app, resources={r'/*': {'origins': ''}})
-
-defaultPath = '/api'
-
-Items = [
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Cake',
-        'type': 'Dessert'
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Pizza',
-        'type': 'Dinner'
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'name': 'Green Eggs and Ham',
-        'type': 'Breakfast'
-    }
-]
+app = create_app()
 
 
-@app.route(defaultPath + '/items', methods=['GET', 'POST'])
-def all_items():
-    response_object = {'status': 'success'}
-    if request.method == 'POST':
-        post_data = request.get_json()
-        item = {
-            'id': uuid.uuid4().hex,
-            'name': post_data.get('name'),
-            'type': post_data.get('type'),
-        }
-        Items.append(item)
-        response_object['message'] = 'Item added!'
-        response_object['obj'] = item
-    else:
-        response_object['items'] = Items
-    return jsonify(response_object)
-
-
-@app.route(defaultPath + '/items/<item_id>', methods=['PUT', 'DELETE'])
-def single_item(item_id):
-    response_object = {'status': 'success'}
-    if request.method == 'PUT':
-        post_data = request.get_json()
-        edit_item(item_id, post_data)
-        response_object['message'] = 'Item updated!'
-    if request.method == 'DELETE':
-        remove_item(item_id)
-        response_object['message'] = 'Item removed!'
-    return jsonify(response_object)
-
-
-def remove_item(item_id):
-    for item in Items:
-        if item['id'] == item_id:
-            Items.remove(item)
-            return True
-    return False
-
-
-def edit_item(item_id, updated_item):
-    for item in Items:
-        if item['id'] == item_id:
-            item['name'] = updated_item['payload']['name']
-            item['type'] = updated_item['payload']['type']
-            return True
-    return False
-
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/apple-touch-icon.png', methods=['GET'])
-def favicon_apple():
-    return send_from_directory(app.template_folder, 'apple-touch-icon.png')
-
-
-@app.route('/favicon-32x32.png', methods=['GET'])
-def favicon_32():
-    return send_from_directory(app.template_folder, 'favicon-32x32.png')
-
-
-@app.route('/favicon-16x16.png', methods=['GET'])
-def favicon_16():
-    return send_from_directory(app.template_folder, 'favicon-16x16.png')
-
-
-@app.route('/site.webmanifest', methods=['GET'])
-def manifest():
-    return send_from_directory(app.template_folder, 'site.webmanifest')
+def main():
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
