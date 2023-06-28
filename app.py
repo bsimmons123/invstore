96a257a1b4a343c54e76312c377deec27a23e092
Brandon Simmons <bsimmons1@ycp.edu>
2023-06-27 21:11:37 -0400

    fixing nginx routing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask_cors import CORS
+
 from application import create_app
 
 app = create_app()
@@ -5,6 +7,9 @@ app = create_app()
 
 def main():
     app.run()
+
+    # enable CORS
+    CORS(app, resources={r'/*': {'origins': ''}})
 
 
 if __name__ == '__main__':
